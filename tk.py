0f7d91791348b2c67850bde284570598f497a8da
axolp <j.w.ploska@gmail.com>
2024-06-16 19:56:37 +0200

    fix:cores, freq

--- tk.py
+++ tk.py
@@ -7,26 +7,30 @@ from tkinter import ttk
 import konsolowa as k  # Zaimportuj odpowiedni moduł
 
 
-bm = k.BatteryUsageReader('C:\\Users\\janax\\OneDrive\\Pulpit\\SM\\final version\\sm\\bm_argument.bat', "xiomi", "link to backend", 5)
-
+bm = k.BatteryUsageReader('C:\\Users\\PC\\Desktop\\sm\\sm\\bm_argument.bat', "xiomi", "link to backend", 5)
+cores = 8
+alpha = 2.3
 def read_phone_power():
     cpu_power= 3.993+5.06+4.4
     #odczytac dane o procesorze z apliakcji od michala, ona zapisuje w pobranych data.txt
     return cpu_power
+
 def thread(script_path, device_id, process_name):
     global stop_thread
     while not stop_thread:
         print("wartosc thread: ", stop_thread)
         result = subprocess.run([script_path, device_id, process_name], capture_output=True, text=True)
         data = result.stdout.split("\n")
+        print(data)
         cpu_usage = 0
         for d in data:
             if device_id in d:
-                cpu_usage = float(d.split(" ")[4])
+                cpu_usage = float(d.split(" ")[3])
         print("cpu usage: ", cpu_usage)
-        print("Zuzycie energi: ", (cpu_usage / 100) * read_phone_power(), "mA")
+        power_usage= (cpu_usage / 100) * read_phone_power()*cores*alpha
+        print("Zuzycie energi: ", power_usage, "mA")
         date = datetime.datetime.now()
-        bm.send_data("https://battery-metter-backend.azurewebsites.net/api/measurement/add/", date, device_id, cpu_usage)
+        bm.send_data("https://battery-metter-backend.azurewebsites.net/api/measurement/add/", date, device_id, power_usage)
         time.sleep(5)
     
     print("Pomiar zakonczony")
@@ -37,7 +41,7 @@ def start_thread():
     global stop_thread
     print("urucham pomiary watek")
     stop_thread = False
-    bat_thread = threading.Thread(target=thread, args=("C:\\Users\\janax\\OneDrive\\Pulpit\\SM\\final version\\sm\\bm_argument.bat", combobox1.get(), combobox.get()))
+    bat_thread = threading.Thread(target=thread, args=("C:\\Users\\PC\\Desktop\\sm\\sm\\bm_argument.bat", combobox1.get(), combobox.get()))
     bat_thread.start()  # Start wątku uruchamiającego plik .bat
 
 def stop_thread_func():
@@ -82,7 +86,7 @@ def b_start_measurement():
     text = f"Wprowadzono: , Wybrano urządzenie: {combobox.get()}, Aplikację: {combobox1.get()}"
     label.config(text=text)
     print("Rozpoczęcie pomiaru dla:", combobox1.get())
-    bm.run("C:\\Users\\janax\\OneDrive\\Pulpit\\SM\\final version\\sm\\bm_argument.bat", combobox1.get(), combobox.get())
+    bm.run("C:\\Users\\PC\\Desktop\\sm\\sm\\bm_argument.bat", combobox1.get(), combobox.get())
     #process = subprocess.Popen(["C:\\Users\\PC\\Desktop\\adb\\bm_argument.bat", combobox1.get(), combobox.get()])
 
 def b_stop_measurement():
